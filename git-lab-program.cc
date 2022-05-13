48850fa5829320c8b2bf8fcd0b91d529e4e767aa
Daniel Reeves <dr300221@ohio.edu>
2022-05-13 18:45:23 -0400

    First program done!

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Daniel Reeves
+ *        Date: 05/13/2022
+ * Description: First homework summer
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello, Git!!" << endl;
+    return 0;
+}// main
